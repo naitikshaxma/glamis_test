31ad166b352cd58b65ab5fcf63ac5b55d0429ba2
ShubhChaturvedi <chaturvedishubh650@gmail.com>
2024-06-14 16:21:04 +0530

    ADD : rate limit

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const cors = require("cors")
 const cookieParser = require("cookie-parser")
+const {RateLimiter15mins} = require("./utils/RateLimiter.js")
 
 const app = express()
 
@@ -20,6 +21,7 @@ app.use("/api/v1/users", userRouter)
 
 
 // health check route
+app.use(RateLimiter15mins)
 
 app.get("/api/v1/healthCheck", (req, res)=>{
     res.status(200).json({
