5ef767c3b93d6e494c3edc22524970e556998e0b
Eternatus29 <anikroy173@gmail.com>
2024-06-14 17:04:01 +0530

    added OTP page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
+import EnterOTP from "./pages/EnterOTP";
 
 const App = () => {
     return (
         <Router>
-                <Routes>
-                    <Route path="/login" element={<LoginPage />} />
-                    <Route path="/" element={<SignupPage />} />
-                </Routes>
+            <Routes>
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/" element={<SignupPage />} />
+                <Route path="/enter-otp" element={<EnterOTP />} />
+            </Routes>
         </Router>
     );
 };
