c6767dbab103d946f7f00f8fecb7ea7ed6f132cc
ShubhChaturvedi <chaturvedishubh650@gmail.com>
2024-06-14 15:28:28 +0530

    ADD : login and verify email

--- backend/src/routes/user.router.js
+++ backend/src/routes/user.router.js
@@ -1,11 +1,12 @@
 const { Router } = require("express")
-const { login, logout, refreshAccessToken, signup } = require("../controllers/user.controller.js");
+const { login, logout, refreshAccessToken, signup, verifyEmail } = require("../controllers/user.controller.js");
 const { isAuthenticated }  = require("../middlewares/auth.middleware.js")
 
 const router = Router()
 
 router.route("/signup").post(signup)
 router.route("/login").post(login)
+router.route("/verify-email").post(verifyEmail)
 
 // Authenticated Routes:
 
