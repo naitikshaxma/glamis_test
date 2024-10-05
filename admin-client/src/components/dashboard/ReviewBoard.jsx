b46fcb7afa52209bb886288582a8d81a166fd708
Vinayak Singh <vinsyak318@gmail.com>
2024-10-05 10:57:31 +0530

    button added

--- admin-client/src/components/dashboard/ReviewBoard.jsx
+++ admin-client/src/components/dashboard/ReviewBoard.jsx
@@ -1,89 +1,9 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState} from 'react';
 import {Card, Typography} from "@material-tailwind/react";
 import {Link, useSearchParams} from 'react-router-dom';
 import axios from 'axios';
 
 const TABLE_HEAD = ["S.no", "Company", "Date", "Slot", "Candidates", "Status", "Action"];
-
-const TABLE_ROWS = [
-  {
-    InterviewId: 1,
-    Company: "JP Morgan",
-    Date: "12/08/2024",
-    Slot: "10:00-11:00",
-    Candidates: 59,
-    Status: "Scheduled"
-
-  },
-  {
-    InterviewId: 2,
-    Company: "Tech Mahindra",
-    Date: "12/08/2024",
-    Slot: "09:00-11:00",
-    Candidates: 200,
-    Status: "Completed"
-  },
-  {
-    InterviewId: 3,
-    Company: "Capgimini",
-    Date: "12/08/2024",
-    Slot: "10:00-11:00",
-    Candidates: 15,
-    Status: "Scheduled"
-  },
-  {
-    InterviewId: 4,
-    Company: "TCS",
-    Date: "12/08/2024",
-    Slot: "10:00-11:00",
-    Candidates: 500,
-    Status: "Completed"
-  },
-  {
-    InterviewId: 5,
-    Company: "Accenture",
-    Date: "12/08/2024",
-    Slot: "10:00-11:00",
-    Candidates: 50,
-    Status: "Scheduled"
-  }
-];
-
-const TABLE_HEAD2 = ["Rank", "Name", "Score", "Company"];
-
-const TABLE_ROWS2 = [
-  {
-    Rank: 1,
-    Name: "John Doe",
-    Score: 90,
-    Company: "JP Morgan"
-  },
-  {
-    Rank: 2,
-    Name: "Jane Doe",
-    Score: 80,
-    Company: "Tech Mahindra"
-  },
-  {
-    Rank: 3,
-    Name: "John Doe",
-    Score: 70,
-    Company: "Capgimini"
-  },
-  {
-    Rank: 4,
-    Name: "John Doe",
-    Score: 60,
-    Company: "TCS"
-  },
-  {
-    Rank: 5,
-    Name: "John Doe",
-    Score: 50,
-    Company: "Accenture"
-  }
-];
-
 
 export default function ReviewBoard() {
   const [search, setSearch] = useSearchParams();
@@ -91,6 +11,7 @@ export default function ReviewBoard() {
   const [completedInterviews, setCompletedInterviews] = useState(0);
   const [pendingInterviews, setPendingInterviews] = useState(0);
   const [interviewDetails, setInterviewDetails] = useState([]);
+
   const fetchInterviewStatusCount = async () => {
     try {
       const res = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/v1/admin/interview/fetchInterviewStatusCount`, {}, {
@@ -105,7 +26,7 @@ export default function ReviewBoard() {
       const data = {
         page: search.get('page') || 1,
         limit: search.get('limit') || 10,
-      }
+      };
       const response = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/v1/admin/interview/fetchInterviewDetails`, data, {
         headers: {
           "Content-Type": "application/json",
@@ -113,53 +34,56 @@ export default function ReviewBoard() {
       });
 
       setInterviewDetails(response.data.interviews);
-
     } catch (err) {
       console.log(err);
     }
-  }
+  };
+
   useEffect(() => {
     fetchInterviewStatusCount();
   }, [search.get('page'), search.get('limit')]);
+
   useEffect(() => {
     fetchInterviewStatusCount();
   }, []);
+
   return (
     <div>
       <div className="flex flex-col p-6 bg-white rounded-lg">
-        <div className="flex  justify-between w-full border-b pb-2">
+        <div className="flex justify-between w-full border-b pb-2">
           <h1 className="text-2xl font-semibold mb-4">Review Board</h1>
         </div>
         <div className="flex justify-between w-full">
           <div className="w-1/3">
-            <div className='flex flex-col m-3 border p-3 rounded-lg bg-gray-100'>
-              <p className='text-sm font-semibold'>Total Interview Scheduled</p>
-              <p className='text-2xl font-semibold text-green-900'>{scheduledInterviews}</p>
+            <div className="flex flex-col m-3 border p-3 rounded-lg bg-gray-100">
+              <p className="text-sm font-semibold">Total Interview Scheduled</p>
+              <p className="text-2xl font-semibold text-green-900">{scheduledInterviews}</p>
             </div>
           </div>
           <div className="w-1/3">
-            <div className='flex flex-col m-3 border p-3 rounded-lg bg-gray-100'>
-              <p className='text-sm font-semibold'>Total Interview Conducted</p>
-              <p className='text-2xl font-semibold text-yellow-900'>{completedInterviews}</p>
+            <div className="flex flex-col m-3 border p-3 rounded-lg bg-gray-100">
+              <p className="text-sm font-semibold">Total Interview Conducted</p>
+              <p className="text-2xl font-semibold text-yellow-900">{completedInterviews}</p>
             </div>
           </div>
           <div className="w-1/3">
-            <div className='flex flex-col m-3 border p-3 rounded-lg bg-gray-100'>
-              <p className='text-sm font-semibold'>Total Interview Pending</p>
-              <p className='text-2xl font-semibold text-red-900'>{pendingInterviews}</p>
+            <div className="flex flex-col m-3 border p-3 rounded-lg bg-gray-100">
+              <p className="text-sm font-semibold">Total Interview Pending</p>
+              <p className="text-2xl font-semibold text-red-900">{pendingInterviews}</p>
             </div>
           </div>
         </div>
+
         <div className="flex justify-between w-full">
-          <div className='w-2/3'>
+          <div className="w-2/3">
             <div className="flex flex-col m-3 border p-3 rounded-lg bg-gray-100">
-              {/* a table with a filter feature */}
+              {/* Table with a filter feature */}
               <div className="flex justify-between items-center">
                 <div className="flex flex-col gap-2">
                   <h1 className="text-lg font-semibold">Interview Scheduled</h1>
 
-                  {/*  limit */}
-                  <div className={'flex items-center gap-2'}>
+                  {/* Limit filter */}
+                  <div className="flex items-center gap-2">
                     <label htmlFor="limit">Limit</label>
                     <select name="limit" id="limit" className="border border-gray-200 p-2 rounded-lg"
                             onChange={(e) => setSearch(params => {
@@ -174,8 +98,8 @@ export default function ReviewBoard() {
                     </select>
                   </div>
 
-                  {/*  page */}
-                  <div className={'flex items-center gap-2'}>
+                  {/* Page filter */}
+                  <div className="flex items-center gap-2">
                     <label htmlFor="page">Page</label>
                     <select name="page" id="page" className="border border-gray-200 p-2 rounded-lg"
                             onChange={(e) => setSearch(params => {
@@ -188,21 +112,14 @@ export default function ReviewBoard() {
                     </select>
                   </div>
                 </div>
-                <div className={'flex flex-col'}>
-                  <div className="flex gap-2">
 
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
-                         stroke="currentColor" className="size-6 my-2 cursor-pointer">
-                      <path strokeLinecap="round" strokeLinejoin="round"
-                            d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75"/>
-                    </svg>
-
-                    <input type="text" placeholder="Search" className="border border-gray-200 p-2 rounded-lg"/>
-                  </div>
-                    {/* download attendance button with multiple selection */}
-
+                {/* Search box */}
+                <div className="flex gap-2">
+                  <input type="text" placeholder="Search" className="border border-gray-200 p-2 rounded-lg"/>
                 </div>
               </div>
+
+              {/* Interview Details Table */}
               <Card className="h-full w-full overflow-scroll my-3">
                 <table className="w-full min-w-max table-auto text-left">
                   <thead>
@@ -224,71 +141,43 @@ export default function ReviewBoard() {
                   </tr>
                   </thead>
                   <tbody>
-                  {interviewDetails.map(({
-                                           company, date, slot, candidates, status, URL, _id
-                                         }, index) => {
-                    const isLast = index === TABLE_ROWS.length - 1;
-                    const classes = isLast ? "p-4" : "p-4 border-b border-blue-gray-50";
+                  {interviewDetails.map(({company, date, slot, candidates, status, _id}, index) => {
+                    const classes = index === interviewDetails.length - 1 ? "p-4" : "p-4 border-b border-blue-gray-50";
 
                     return (
-                      <tr key={name}>
+                      <tr key={_id}>
                         <td className={classes}>
-                          <Typography
-                            variant="small"
-                            color="blue-gray"
-                            className="font-normal"
-                          >
+                          <Typography variant="small" color="blue-gray" className="font-normal">
                             {index + 1 + (search.get('page') - 1) * (search.get('limit') || 10)}
                           </Typography>
                         </td>
                         <td className={classes}>
-                          <Typography
-                            variant="small"
-                            color="blue-gray"
-                            className="font-normal"
-                          >
+                          <Typography variant="small" color="blue-gray" className="font-normal">
                             <Link to="" className='text-blue-500'>{company}</Link>
                           </Typography>
                         </td>
                         <td className={classes}>
-                          <Typography
-                            variant="small"
-                            color="blue-gray"
-                            className="font-normal"
-                          >
+                          <Typography variant="small" color="blue-gray" className="font-normal">
                             {date.split('T')[0]}
                           </Typography>
                         </td>
                         <td className={classes}>
-                          <Typography
-                            as="a"
-                            href="#"
-                            variant="small"
-                            color="blue-gray"
-                            className="font-medium"
-                          >
+                          <Typography as="a" href="#" variant="small" color="blue-gray" className="font-medium">
                             {slot}
                           </Typography>
                         </td>
                         <td className={classes}>
-                          <Typography
-                            variant="small"
-                            color="blue-gray"
-                            className="font-normal"
-                          >
+                          <Typography variant="small" color="blue-gray" className="font-normal">
                             {candidates}
                           </Typography>
                         </td>
                         <td className={classes}>
-                          <Typography
-                            variant="small"
-                            color={status === "Ended" ? "red" : "orange"}
-                            className="font-normal"
-                          >
+                          <Typography variant="small" color={status === "Ended" ? "red" : "orange"} className="font-normal">
                             {status}
                           </Typography>
                         </td>
                         <td className={classes}>
+                          {/* Download Attendance Button */}
                           <a target="_blank"
                              href={`${import.meta.env.VITE_BACKEND_URL}/api/v1/admin/interview/downloadAttendance/?interviewId=${_id}`}>
                             <button className="bg-blue-500 text-white p-2 rounded-lg">Download<br/>Attendance</button>
@@ -302,93 +191,8 @@ export default function ReviewBoard() {
               </Card>
             </div>
           </div>
-
-          <div className='w-1/3'>
-            <div className="flex flex-col m-3 border p-3 rounded-lg bg-gray-100">
-              <div className="flex justify-between">
-                <h1 className="text-lg font-semibold">Top students leaderboard</h1>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
-                     stroke="currentColor" className="size-6 my-2 cursor-pointer">
-                  <path strokeLinecap="round" strokeLinejoin="round"
-                        d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75"/>
-                </svg>
-              </div>
-              <Card className="h-full w-full overflow-scroll my-3">
-                <table className="w-full min-w-max table-auto text-left">
-                  < thead>
-                  <tr>
-                    {TABLE_HEAD2.map((head) => (
-                      <th
-                        key={head}
-                        className="border-b border-blue-gray-100 bg-blue-gray-50 p-4"
-                      >
-                        <Typography
-                          variant="small"
-                          color="blue-gray"
-                          className="font-normal leading-none opacity-70"
-                        >
-                          {head}
-                        </Typography>
-                      </th>
-                    ))}
-                  </tr>
-                  </thead>
-
-                  <tbody>
-                  {TABLE_ROWS2.map(({Rank, Name, Score, Company}, index) => {
-                    const isLast = index === TABLE_ROWS2.length - 1;
-                    const classes = isLast ? "p-4" : "p-4 border-b border-blue-gray-50";
-
-                    return (
-                        <tr key={Name}>
-                        <td className={classes}>
-                          <Typography
-                            variant="small"
-                            color="blue-gray"
-                            className="font-normal"
-                          >
-                            {Rank}
-                          </Typography>
-                        </td>
-                        <td className={classes}>
-                          <Typography
-                            variant="small"
-                            color="blue-gray"
-                            className="font-normal"
-                          >
-                            {Name}
-                          </Typography>
-                        </td>
-                        <td className={classes}>
-                          <Typography
-                            variant="small"
-                            color="blue-gray"
-                            className="font-normal"
-                          >
-                            {Score}
-                          </Typography>
-                        </td>
-                        <td className={classes}>
-                          <Typography
-                            variant="small"
-                            color="blue-gray"
-                            className="font-normal"
-                          >
-                            {Company}
-                          </Typography>
-                        </td>
-                      </tr>
-                    );
-                  })}
-                  </tbody>
-
-                </table>
-              </Card>
-            </div>
-          </div>
-
         </div>
       </div>
     </div>
-  )
+  );
 }
