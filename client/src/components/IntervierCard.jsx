bf088b2a1486c179bac844e1981fe67c52e52379
Krish Agrawal <printf.krish@gmail.com>
2024-10-08 01:35:57 +0530

    optimised IntervierCard.jsx

--- client/src/components/IntervierCard.jsx
+++ client/src/components/IntervierCard.jsx
@@ -1,168 +1,124 @@
 import {
-    Card,
-    CardBody,
-    CardFooter,
-    Typography,
-    Button,
+  Card, CardBody, CardFooter, Typography, Button,
 } from "@material-tailwind/react";
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import Cookies from 'js-cookie'
-import axios from 'axios';
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
+import {bearerInstance as instance} from "../helpers/instance";
+// import {toast} from "react-toastify";
 
-export default function InterviewCard({ props, status }) {
-    const navigate = useNavigate();
-    const [totalQuestions, setTotalQuestions] = useState(15);
-    const [company, setCompany] = useState();
-    const [adminInterviewId, setAdminInterviewId] = useState();
+export default function InterviewCard({props, status}) {
+  const navigate = useNavigate();
+  const [totalQuestions, setTotalQuestions] = useState(15);
+  const [company, setCompany] = useState();
+  const [adminInterviewId, setAdminInterviewId] = useState();
 
 
-    const handleInterview = async (e) => {
-        // clear all cookies
-        Cookies.remove('interviewId');
-        Cookies.remove('subject');
-        Cookies.remove('jobTitle');
-        Cookies.remove('selectedCompany');
-        Cookies.remove('adminInterviewId');
-        Cookies.remove('delta');
-        Cookies.remove('verbal');
-        Cookies.set('interviewId', props._id);
-        if (props.type === 'subject') {
-            Cookies.set('subject', props.description);
-        }
-        else if(props.type === 'written') {
-            Cookies.set('subject', props.description);
-        }
-        else if(props.type === 'verbal') {
-            Cookies.set('verbal', true);
-        }
-         else {
-            Cookies.set('jobTitle', props.title);
-            Cookies.set('selectedCompany', company);
-        }
-        Cookies.set('adminInterviewId', adminInterviewId);
-        Cookies.set('delta', totalQuestions);
+  const handleInterview = async () => {
+    let url = '';
+    let redirect = '';
+    const removeCookies = ['interviewId', 'subject', 'jobTitle', 'selectedCompany', 'adminInterviewId', 'delta', 'verbal'];
+    const newCookies = {interviewId: props._id, adminInterviewId, delta: totalQuestions};
 
-        localStorage.setItem('jd', props.description);
-        if(props.type === 'written') {
-            const response = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/v1/interview/createInterviewByWrittenAdmin`, {
-                interviewId: props._id
-            }, {
-                headers: {
-                    "content-type": "application/json",
-                    "Authorization": `Bearer ${Cookies.get('accessToken')}`
-                }
-            },)
-            navigate('/written');
-            return;
-        }
-        else if(props.type === 'verbal'){
-            const response = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/v1/interview/createInterviewByVerbalAdmin`, {
-                interviewId: props._id
-            }, {
-                headers: {
-                    "content-type": "application/json",
-                    "Authorization": `Bearer ${Cookies.get('accessToken')}`
-                }
-            },)
-            navigate('/live');
-            return;
-        }
-        else {
-            const response = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/v1/interview/createInterviewByJDAdmin`, {
-                interviewId: props._id
-            }, {
-                headers: {
-                    "content-type": "application/json",
-                    "Authorization": `Bearer ${Cookies.get('accessToken')}`
-                }
-            },)
-        }
-        
-        navigate('/live');
+    if (props.type === 'subject') {
+      newCookies.subject = props.description;
     }
 
-    const fetchAdminInterviewDetails = async() => {
-        const response = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/v1/admin/interview/fetch`, {
-            interviewId: props._id
-        }, {
-            headers: {
-                "content-type": "application/json",
-                "Authorization": `Bearer ${Cookies.get('accessToken')}`
-            }
-        });
-        
-        console.log(response.data.adminInterviewId);
-
-        setCompany(response.data.company);
-        setAdminInterviewId(response.data.adminInterviewId);
-        setTotalQuestions(response.data.totalQuestions);
+    if (props.type === 'written') {
+      newCookies.subject = props.description;
+      url = '/api/v1/interview/createInterviewByWrittenAdmin';
+      redirect = '/written';
+    }
+    if (props.type === 'company') {
+      newCookies.jobTitle = props.title;
+      newCookies.selectedCompany = company;
+      url = '/api/v1/interview/createInterviewByCompanyAdmin';
+      redirect = '/live';
     }
 
-    useEffect(() => {
-        fetchAdminInterviewDetails();
-    }, []);
+    if (props.type === 'verbal') {
+      newCookies.verbal = true;
+      url = '/api/v1/interview/createInterviewByVerbalAdmin';
+      redirect = '/live';
+    }
 
-    return (
-        <Card className="m-4 h-fit w-1/4" >
-            <CardBody>
-                <Typography color="blue-gray" className="mb-2">
-                    {/* heading of inerview */}
-                    <div className="font-bold">{
-                        props.title
-                    }</div>
-                </Typography>
-                <Typography color="blue-gray">
-                    {/* description of interview */}
-                    <div className="my-2">
+    if (props.type === 'Svar') {
+      url = '/api/v1/interview/createInterviewBySvarAdmin';
+      redirect = '/live';
 
-                        <i className='bx bx-broadcast bx-tada bx-rotate-90' ></i>
-                        {/* active now */}
-                        {/* <span className="text-[#2b6030] font-bold mx-1"> {
+    }
+
+    removeCookies.forEach(cookie => Cookies.remove(cookie));
+    Object.entries(newCookies).forEach(([key, value]) => Cookies.set(key, value));
+    localStorage.setItem('jd', props.description);
+
+
+
+    await instance.post(url, {interviewId: props._id}); // todo: fix response NOT used
+    navigate(redirect);
+  }
+  const fetchAdminInterviewDetails = async () => {
+    const response = await instance.post(`/api/v1/admin/interview/fetch`, {interviewId: props._id});
+    console.log(response.data.adminInterviewId);
+
+    setCompany(response.data.company);
+    setAdminInterviewId(response.data.adminInterviewId);
+    setTotalQuestions(response.data.totalQuestions);
+  }
+
+  useEffect(() => {
+    fetchAdminInterviewDetails();
+  }, []);
+
+  return (<Card className="m-4 h-fit w-1/4">
+    <CardBody>
+      <Typography color="blue-gray" className="mb-2">
+        {/* heading of inerview */}
+        <div className="font-bold">{props.title}</div>
+      </Typography>
+      <Typography color="blue-gray">
+        {/* description of interview */}
+        <div className="my-2">
+
+          <i className='bx bx-broadcast bx-tada bx-rotate-90'></i>
+          {/* active now */}
+          {/* <span className="text-[#2b6030] font-bold mx-1"> {
                     props.status }</span> */}
-                        {props.status === 'active now' ? (
-                            <span className="text-[#2b6030] font-bold mx-1"> {
-                                props.status}</span>) : props.status === 'Upcoming Interview' ? (
-                                    <span className="text-[#2b6030] font-bold mx-1"> {
-                                        props.status}</span>) : (
-                            <span className="text-[#2b6030] font-bold mx-1"> {
-                                props.status}</span>
-                        )
-                        }
-                    </div>
+          {props.status === 'active now' ? (<span
+            className="text-[#2b6030] font-bold mx-1"> {props.status}</span>) : props.status === 'Upcoming Interview' ? (
+            <span className="text-[#2b6030] font-bold mx-1"> {props.status}</span>) : (
+            <span className="text-[#2b6030] font-bold mx-1"> {props.status}</span>)}
+        </div>
 
-                    <hr />
-                    <div className="flex justify-between my-2">
-                        <div>
-                            <span className="font-bold">Date:</span> {props.start_time?.split('T')[0]}
-                        </div>
-                        <div>
-                            <span className="font-bold">Time:</span> {props.start_time?.split('T')[1].split('.')[0]}
-                        </div>
-                    </div>
-                    <div className="my-2">
-                        <div>
-                            <span className="font-bold">Duration:</span> {(new Date(props.end_time) - new Date(props.start_time)) / 1000 / 60} minutes
-                        </div>
-                    </div>
-                </Typography>
+        <hr/>
+        <div className="flex justify-between my-2">
+          <div>
+            <span className="font-bold">Date:</span> {props.start_time?.split('T')[0]}
+          </div>
+          <div>
+            <span className="font-bold">Time:</span> {props.start_time?.split('T')[1].split('.')[0]}
+          </div>
+        </div>
+        <div className="my-2">
+          <div>
+                                <span
+                                  className="font-bold">Duration:</span> {(new Date(props.end_time) - new Date(props.start_time)) / 1000 / 60} minutes
+          </div>
+        </div>
+      </Typography>
 
-            </CardBody>
-            <CardFooter>
-                {status === 'active now' ? (
-                    <Button color="lightGreen"
-                        className="w-full bg-[#2b6030] hover:bg-[#1c3d1f]" onClick={handleInterview}
-                    >Join Interview</Button>
-                ) : status === 'Upcoming Interview' ? (
-                    <Button color="lightGreen"
-                            className="w-full bg-yellow-900 hover:bg-yellow-800" disabled
-                    >Coming Soon</Button>
-                ) : (
-                    <Button color="lightGreen"
-                        className="w-full bg-red-900 hover:bg-red-800" onClick={() => navigate(`/history/detailed/${props._id}`)}
-                    >View Result</Button>
-                )}
-            </CardFooter>
-        </Card>
-    );
+    </CardBody>
+    <CardFooter>
+      {status === 'active now' ? (<Button color="lightGreen"
+                                          className="w-full bg-[#2b6030] hover:bg-[#1c3d1f]"
+                                          onClick={handleInterview}
+      >Join Interview</Button>) : status === 'Upcoming Interview' ? (<Button color="lightGreen"
+                                                                             className="w-full bg-yellow-900 hover:bg-yellow-800"
+                                                                             disabled
+      >Coming Soon</Button>) : (<Button color="lightGreen"
+                                        className="w-full bg-red-900 hover:bg-red-800"
+                                        onClick={() => navigate(`/history/detailed/${props._id}`)}
+      >View Result</Button>)}
+    </CardFooter>
+  </Card>);
 }
