3645a36fd7e21905a479ac109c377b7eeff4ca90
Krish Agrawal <printf.krish@gmail.com>
2024-10-09 13:49:50 +0530

    fixed errors of interview.router.js

--- backend/src/routes/interview.router.js
+++ backend/src/routes/interview.router.js
@@ -43,8 +43,8 @@ router.route("/generateQuestionForWrittenAdmin").post(isAuthenticated, generateQ
 router.route("/createInterviewByWrittenAdmin").post(isAuthenticated, createInterviewByWrittenAdmin);
 router.route("/createInterviewByVerbalAdmin").post(isAuthenticated, createInterviewByVerbalAdmin);
 router.route("/generateQuestionForVerbalAdmin").post(isAuthenticated, generateQuestionForVerbalAdmin);
-router.route("/generateQuestionForSvarAdmin").post(isAuthenticated, generateQuestionforSvarAdmin); 
-router.route("createInterviewBySvarAdmin").post(isAuthenticated, createInterviewBySvarAdmin)
+router.route("/generateQuestionForSvarAdmin").post(isAuthenticated, generateQuestionforSvarAdmin);
+router.route("/createInterviewBySvarAdmin").post(isAuthenticated, createInterviewBySvarAdmin)
 
 export default router;
 
