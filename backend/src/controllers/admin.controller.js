a901bccbf7bb4035a3c8e98b662f99e4927cd96d
yashasvinitripathi11 <yashasvinitripathi@gmail.com>
2024-12-25 14:30:57 +0530

    changes done

--- backend/src/controllers/admin.controller.js
+++ backend/src/controllers/admin.controller.js
@@ -645,94 +645,98 @@ export const downloadAttendance = async (req, res) => {
       }
     }
 
-    const pipeline = [
-      // Stage 1: Match specific start_time and is_active false
+    // const pipeline = [
+    //   // Stage 1: Match specific start_time and is_active false
 
-      {
-        $match: {
-          _id: { $in: interviews },
-        }
-      },
-      // Stage 2: Project the _id field of the interviews and attemptedQuestions
-      {
-        $project: { _id: 1, is_active: 1, attemptedQuestions: 1 }
-      },
-      // Stage 3: Lookup in students collection using interview _id
-      {
-        $lookup: {
-          from: "students",
-          let: { interviewId: "$_id" },
-          pipeline: [
-            {
-              $match: {
-                $expr: {
-                  $and: [
-                    { $in: ["$$interviewId", { $ifNull: ["$interview_taken", []] }] }
-                  ]
-                }
-              }
-            }
-          ],
-          as: "matched_students"
-        }
-      },
-      // Stage 4: Unwind the matched_students array
-      {
-        $unwind: "$matched_students"
-      },
-      // Stage 5: Project the user_id from matched students
-      {
-        $project: {
-          user_id: "$matched_students.user",
-          is_active: 1,
-          attemptedQuestions: 1,
-          InterviewId: "$_id"
-        }
-      },
-      // Stage 6: Lookup in users collection using user_id
-      {
-        $lookup: {
-          from: "users",
-          localField: "user_id",
-          foreignField: "_id",
-          as: "user_data"
-        }
-      },
-      // Stage 7: Unwind the user_data array
-      {
-        $unwind: "$user_data"
-      },
-      // Stage 8: Project the email, name, and _id from user_data
-      {
-        $project: {
-          email: "$user_data.email_id",
-          name: "$user_data.name",
-          _id: "$user_data._id",
-          is_active: 1,
-          attemptedQuestions: 1,
-          InterviewId: 1
-        }
-      },
-      // Stage 9: Group results and include count
-      {
-        $group: {
-          _id: null,
-          emails: {
-            $push: {
-              Email: "$email", Name: "$name", Id: "$_id", Present: { $not: "$is_active" }, AttemptedQuestions: "$attemptedQuestions", View_Report: {
-                $concat: ["http://glamis.in/history/detailed/", { $toString: "$InterviewId" }]
-              }
-            }
-          },
-          total_count: { $sum: 1 }
-        }
-      }
+    //   {
+    //     $match: {
+    //       _id: { $in: interviews },
+    //     }
+    //   },
+    //   // Stage 2: Project the _id field of the interviews and attemptedQuestions
+    //   {
+    //     $project: { _id: 1, is_active: 1, attemptedQuestions: 1 }
+    //   },
+    //   // Stage 3: Lookup in students collection using interview _id
+    //   {
+    //     $lookup: {
+    //       from: "students",
+    //       let: { interviewId: "$_id" },
+    //       pipeline: [
+    //         {
+    //           $match: {
+    //             $expr: {
+    //               $and: [
+    //                 { $in: ["$$interviewId", { $ifNull: ["$interview_taken", []] }] }
+    //               ]
+    //             }
+    //           }
+    //         }
+    //       ],
+    //       as: "matched_students"
+    //     }
+    //   },
+    //   // Stage 4: Unwind the matched_students array
+    //   {
+    //     $unwind: "$matched_students"
+    //   },
+    //   // Stage 5: Project the user_id from matched students
+    //   {
+    //     $project: {
+    //       user_id: "$matched_students.user",
+    //       is_active: 1,
+    //       attemptedQuestions: 1,
+    //       InterviewId: "$_id"
+    //     }
+    //   },
+    //   // Stage 6: Lookup in users collection using user_id
+    //   {
+    //     $lookup: {
+    //       from: "users",
+    //       localField: "user_id",
+    //       foreignField: "_id",
+    //       as: "user_data"
+    //     }
+    //   },
+    //   // Stage 7: Unwind the user_data array
+    //   {
+    //     $unwind: "$user_data"
+    //   },
+    //   // Stage 8: Project the email, name, and _id from user_data
+    //   {
+    //     $project: {
+    //       email: "$user_data.email_id",
+    //       name: "$user_data.name",
+    //       _id: "$user_data._id",
+    //       is_active: 1,
+    //       attemptedQuestions: 1,
+    //       InterviewId: 1
+    //     }
+    //   },
+    //   // Stage 9: Group results and include count
+    //   {
+    //     $group: {
+    //       _id: null,
+    //       emails: {
+    //         $push: {
+    //           Email: "$email", Name: "$name", Id: "$_id", Present: { $not: "$is_active" }, AttemptedQuestions: "$attemptedQuestions", View_Report: {
+    //             $concat: ["http://glamis.in/history/detailed/", { $toString: "$InterviewId" }]
+    //           }
+    //         }
 
-    ]
+    //       },
+    //       total_count: { $sum: 1 }
+    //     }
+    //   }
 
-    const students = await Interview.aggregate(pipeline);
+    // ]
 
-    const fields = ['Email', 'Name', 'Id', 'Present', 'View_Report'];
+ 
+    
+
+    const students = await Interview.aggregate(combinedPipeline);
+
+    const fields = ['Email', 'Name', 'Id', 'Present','TotalScore','AverageScore','View_Report'];
     const opts = { fields };
     const parser = new Parser(opts);
     let arr = [];
